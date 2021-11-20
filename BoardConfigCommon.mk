9f3a395f83f88bdf698a8abcf1583c217960f4b1
Canary233 <2096276316@qq.com>
2021-11-20 11:53:39 +0800

    sdm660-common:disable sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,12 +127,6 @@ TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
-# SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
-
 # Treble
 BOARD_VNDK_VERSION := current
 
